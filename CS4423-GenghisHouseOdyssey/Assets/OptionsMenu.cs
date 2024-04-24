c1005afdb73e2510690a5fca6729e71930150fcb
Oyuki-kintaro <lydia_k_u@yahoo.com>
2024-04-24 01:29:45 -0500

    create options menu

---
+++ CS4423-GenghisHouseOdyssey/Assets/OptionsMenu.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using TMPro;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [Header("Audio")]
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Slider masterVolumeSlider;
+    [SerializeField] Slider musicVolumeSlider;
+
+    [Header("Resolution")]
+    [SerializeField] TMP_Dropdown resDropdown;
+    [SerializeField] Toggle fullscreenToggle;
+    Resolution[] resolutions;
+    [SerializeField] Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resDropdown.value = PlayerPrefs.GetInt("ResolutionDropdown");
+        //fullscreenToggle.isOn = PlayerPrefs.GetInt("FullscreenToggle", 1) == 1;
+        //Screen.SetResolution(resolutions[resDropdown.value].width,resolutions[resDropdown.value].height,fullscreenToggle.isOn);
+
+        masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        GetResolutionOption();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMasterVolume() {
+       audioMixer.SetFloat("MasterVolume", ConvertToDec(masterVolumeSlider.value));
+        PlayerPrefs.SetFloat("MasterVolume", masterVolumeSlider.value);
+        PlayerPrefs.Save();
+    }
+
+    public void SetMusicVolume() {
+        audioMixer.SetFloat("MusicVolume", ConvertToDec(musicVolumeSlider.value));
+        PlayerPrefs.SetFloat("MusicVolume", musicVolumeSlider.value);
+        PlayerPrefs.Save();
+    }
+
+    float ConvertToDec(float sliderValue) {
+        return Mathf.Log10(Mathf.Max(sliderValue, 0.0001f)) * 20;
+    }
+
+    void GetResolutionOption() {
+        resDropdown.ClearOptions();
+        resolutions = Screen.resolutions;
+
+        for(int i = 0; i < resolutions.Length; i++) {
+            TMP_Dropdown.OptionData newOption;
+            newOption = new TMP_Dropdown.OptionData(resolutions[i].width.ToString() + "x" + resolutions[i].height.ToString());
+            resDropdown.options.Add(newOption);
+        }
+    }
+
+    public void ChooseResolution() {
+        Screen.SetResolution(resolutions[resDropdown.value].width,resolutions[resDropdown.value].height,fullscreenToggle.isOn);
+        PlayerPrefs.SetInt("ResolutionDropdown", resDropdown.value);
+        PlayerPrefs.SetInt("FullscreenToggle", fullscreenToggle.isOn ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+
+    public void OpenOptions() {
+        canvas.enabled = true;
+    }
+
+    public void CloseOptions() {
+        canvas.enabled = false;
+    }
+}
